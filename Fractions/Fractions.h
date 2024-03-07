4e4ce82dface4823ea8a27d69518d6f436c677e8
OnlyDushesssOff <144950810+OnlyDushesssOff@users.noreply.github.com>
2024-03-07 12:39:27 +0300

    Update Fractions.h

--- Fractions/Fractions.h
+++ Fractions/Fractions.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <cstdint>
 
@@ -20,5 +21,52 @@ public:
 
     void Print();
 
-};
+    friend std::ostream& operator<< (std::ostream& os, const Fractions& tmp){
+        os << tmp._num << " / " << tmp._denom << std::endl;
+        return os;
+    }   
+    Fractions operator=(const Fractions& fractions){
+        _num = fractions._num;
+        _denom = fractions._denom;
+        return *this;
+    }
 
+    Fractions operator+(const Fractions& fractions) const {
+        int32_t new_num = _num * fractions._denom + _denom * fractions._num;
+        uint32_t new_denom = _denom * fractions._denom;
+        Fractions res = Fractions(new_num, new_denom);
+        return res;
+    }
+
+    Fractions operator-(const Fractions& fractions) const {
+        int32_t new_num = _num * fractions._denom - _denom * fractions._num;
+        uint32_t new_denom = _denom * fractions._denom;
+        Fractions res = Fractions(new_num, new_denom);
+        return res;
+    }
+
+    Fractions operator*(const Fractions& fractions) const {
+        int32_t new_num = _num * fractions._num;
+        uint32_t new_denom = _denom * fractions._denom;
+        Fractions res = Fractions(new_num, new_denom);
+        return res;
+    }
+
+    Fractions operator/(const Fractions& fractions) const {
+        int32_t new_num = _num * fractions._denom + _denom * fractions._num;
+        uint32_t new_denom = _denom * fractions._denom;
+        Fractions res = Fractions(new_num, new_denom);
+        return res;
+    }
+
+    bool operator> (const Fractions& fractions) const {
+        int32_t new_num1 = _num * fractions._denom;
+        int32_t new_num2 = fractions._num * _denom;
+        return new_num1 > new_num2;
+    }
+    bool operator< (const Fractions& fractions) const {
+        int32_t new_num1 = _num * fractions._denom;
+        int32_t new_num2 = fractions._num * _denom;
+        return new_num1 < new_num2;
+    }
+};
